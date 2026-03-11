573762d919742d11d7cf59aafdc4ffc8a1af1ffc
cesar99cruz <95140707+cesar99cruz@users.noreply.github.com>
2026-03-11 15:17:55 +0100

    removal of 2 properties

--- js/properties.js
+++ js/properties.js
@@ -1,36 +1,6 @@
 const properties = [
     {
         title: {
-            pt: "Oportunidade Histórica: Propriedade do Séc. XIII",
-            en: "Historic Opportunity: 13th Century Property"
-        },
-        location: {
-            pt: "Leiria, Portugal",
-            en: "Leiria, Portugal"
-        },
-        price: {
-            pt: "Sob Consulta",
-            en: "Upon Consultation"
-        },
-        type: {
-            pt: "Para Venda",
-            en: "For Sale"
-        },
-        beds: 25,
-        baths: 0,
-        sqm: 1500,
-        image: "assets/properties/DE14/DE14-img1.jpg",
-        link: "property-details.html",
-        ref: "DE 014",
-        status: "Buy",
-        targetType: "Estate",
-        hasParking: true
-    },
-
-    //--------------------------------------------//
-
-    {
-        title: {
             pt: "Terreno com Projeto Aprovado",
             en: "Land with Approved Project"
         },
@@ -55,37 +25,6 @@ const properties = [
         status: "Buy",
         targetType: "Land",
         hasParking: false
-    },
-
-    //--------------------------------------------//
-
-    {
-        title: {
-            pt: "Moradia T3 com Jardim e Estacionamento Privado, a 50 metros da Praia",
-            en: "3-Bedroom House With Garden and Private Parking, 50 Meters From the Beach."
-        },
-        location: {
-            pt: "Parede/Cascais, Lisboa",
-            en: "Parede/Cascais, Lisboa"
-        },
-        price: {
-            pt: "3 827 €",
-            en: "3 827 €"
-        },
-        type: {
-            pt: "Para Arrendar",
-            en: "For Rent"
-        },
-        beds: 3,
-        baths: 3,
-        sqm: 208,
-        image: "assets/properties/DE13/DE13-img1.jpg",
-        link: "property-details.html",
-        ref: "DE 013",
-        status: "Rent",
-        targetType: "Apartment",
-        energyClass: "E",
-        hasParking: true
     },
 
     //--------------------------------------------//
